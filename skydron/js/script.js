a1dc304f06f35a5d293607641c68ca78d2dd90a4
Alexander <a.brednin@mail.ru>
2021-01-26 18:09:31 +0400

    top-header

---
+++ skydron/js/script.js
@@ -0,0 +1,14 @@
+//? Btn catalog, create burger menu and show menu 
+const btnCatalog = document.querySelector('.btn-catalog')
+btnCatalog.addEventListener('click', () => {
+    btnCatalog.classList.toggle('active')
+})
+
+
+//? Btn search if click that show input serch
+
+const btnShowSearch = document.querySelector('.search-width')
+btnShowSearch.addEventListener('click', () => {
+    btnShowSearch.classList.toggle('active')
+})
+   
