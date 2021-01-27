bd96e90134b16db8bf60eb0107ad2779090f616f
Alexander <a.brednin@mail.ru>
2021-01-27 18:02:33 +0400

    new changes

--- skydron/js/script.js
+++ skydron/js/script.js
@@ -1,14 +1,39 @@
 //? Btn catalog, create burger menu and show menu 
+"use strict"
 const btnCatalog = document.querySelector('.btn-catalog')
-btnCatalog.addEventListener('click', () => {
-    btnCatalog.classList.toggle('active')
-})
+btnCatalog.addEventListener('click', () => btnCatalog.classList.toggle('active') )
+    
 
 
 //? Btn search if click that show input serch
-
 const btnShowSearch = document.querySelector('.search-width')
-btnShowSearch.addEventListener('click', () => {
+function showSearch() {
     btnShowSearch.classList.toggle('active')
-})
+}
+btnShowSearch.onclick = function() {
+    showSearch()
+}
+
+
+
+
+
+
+
+
+
    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
