938e77c6d26aaf5c2a2e35bc69d8401c060de9bc
Alexander <a.brednin@mail.ru>
2021-02-01 17:33:08 +0400

    new changes

--- skydron/js/script.js
+++ skydron/js/script.js
@@ -1,40 +1,60 @@
 //? Btn catalog, create burger menu and show menu 
 "use strict"
-const btnCatalog = document.querySelector('.btn-catalog')
-const headerBottom = document.querySelector('.header__bottom')
-const dropdownCatalog = document.querySelector('.header__dropdown-catalog')
-const html = document.querySelector('html')
-const body = document.querySelector('body')
-const elements = [btnCatalog, headerBottom, dropdownCatalog, html, body]
-btnCatalog.addEventListener('click', () => btnCatalogAction())
-const el = document.querySelector(".bottom__wrapper > .container");
-const el2 = document.querySelector(".header__inner > .container");
-const el3 = document.querySelector(".header__dropdown-catalog > .container");
-const getprop = window.getComputedStyle(el, null).getPropertyValue("margin-right");
-let startState = document.documentElement.clientWidth
+
+//!Все элэменты которым добовляется class "active"
+const all_need_tobe_active = document.querySelectorAll([
+    'body',
+    '.header__bottom',
+    '.btn-catalog',
+    '.header__dropdown-catalog',
+])
+
+//! Все контейнеры Top-HEADER
+const containers = document.querySelectorAll(
+    [
+        ".header__top > .container",
+        ".header__bottom > .container",
+    ]
+)
+
+//! Получаем Margin-right у контецнера ".bottom__wrapper > .container".
+const initialGetprop = window.getComputedStyle(containers[0], null).getPropertyValue("margin-right");
+
+
+//!Находим из массива класс '.btn-catalog' и добавляем ему класс "active"
+all_need_tobe_active[2].addEventListener('click', () => btnCatalogAction())
+
+
+
+
+
+
+//! Начальная ширина всего экрана
+let start_W_W = document.documentElement.clientWidth
+
+function locationReload() {
+    location.reload()
+}
+
+//! Активная кнопка Каталога 
 function btnCatalogAction() {
-    elements.forEach(e => e.classList.toggle('active'))
-    let changeState = document.documentElement.clientWidth;
-    let newState = changeState - startState
-    let gg = getprop
-    let ll = Number(gg.slice(0, -2)) + newState
-    el.style.marginRight = ll + 'px'
-    el2.style.marginRight = ll + 'px'
-    el3.style.marginRight = ll + 'px'
+    //!Добавляем всем элементам class "active"
+    all_need_tobe_active.forEach(e => e.classList.toggle('active'))
+    //! Измененная ширина всего экрана
+    let modified_W_W = document.documentElement.clientWidth;
+    let newState = modified_W_W - start_W_W
+    let newGetprop = Number(initialGetprop.slice(0, -2)) + newState
+    containers.forEach(e => e.style.marginRight = newGetprop + 'px')
 }
 
-
-
-console.log()
-
-window.onclick = e => {
-    console.log(e)
-}
+window.addEventListener('resize', locationReload)
 
 
 
 
-//? Btn search if onmouseenter that show input serch
+
+
+//! Btn search if onmouseenter that show input serch
 const btnShowSearch = document.querySelector('.search-width')
 let t
 
@@ -57,9 +77,6 @@ btnShowSearch.onmouseleave = function () {
 
 btnShowSearch.onmouseenter = function (event) {
     event.target.oninput = function () {
-        //let $el = event.target.querySelector('.search').value
-        //document.querySelector('.modalWindow__text').innerHTML =  $el + '  Люблю Анька моя !!!!!!'
-        //console.log($el)
         showSearch()
     }
     event.target.onkeyup = function (event) {
@@ -72,3 +89,12 @@ btnShowSearch.onmouseenter = function (event) {
 
     showSearch()
 }
+
+
+
+
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+})
